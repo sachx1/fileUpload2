9ab6975ceacc3c940775e2463142d71c7e0563fe
Sachin Khargie <sachx1281@gmail.com>
2021-08-20 17:58:07 -0400

    migration

---
+++ credentials.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyA3inDvb7a8PfS2oh2zJWdZ5A6ceEPah5s",
+    authDomain: "apil-experiments.firebaseapp.com",
+    projectId: "apil-experiments",
+    storageBucket: "apil-experiments.appspot.com",
+    messagingSenderId: "842257295791",
+    appId: "1:842257295791:web:7a6a908456dfa97a89a75a",
+    measurementId: "G-WG5B9MZGBE"
+};
+
+export default config;
---
+++ firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import config from './credentials.js';
+
+const firebaseConfig = config;
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+auth.useEmulator()
+
+export const db = firebase.firestore();
+
